7037c63f92f71f40bd16fd1c5aa668931de4956e
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2025-07-18 14:40:29 -0300

    feat: configure logger and update default port in main bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { applyGlobalResources } from './global-resources';
+import { ConsoleLogger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: new ConsoleLogger(),
+  });
 
   applyGlobalResources(app);
 
-  await app.listen(process.env.PORT ?? 3050);
+  await app.listen(process.env.PORT ?? 3002);
 }
 
 bootstrap();
